abc327e0ea1d367833471254d801617808b2e33e
Gary <gary_markoski@hotmail.com>
2022-01-28 14:41:32 -0700

    added lifecyles to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,74 +1,117 @@
 import React, { Component } from "react";
-
-import classnames from "classnames";
 import Loading from "./Loading";
+import classnames from "classnames";
 import Panel from "./Panel";
+import axios from "axios";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
+ import { setInterview } from "helpers/reducers";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6,
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm",
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday",
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3",
-  },
+    getValue: getInterviewsPerDay
+  }
 ];
 
 class Dashboard extends Component {
-  state = {
-    loading: false,
+  state = { 
+    loading: true,
     focused: null,
-  };
+    days: [],
+    appointments: {},
+    interviewers: {}
+   };
 
-  //We are intentionally using this form to highlight a pitfall. Make sure that the selectPanel method is written using the same syntax as below.
-  // selectPanel(id) {
-  //   this.setState({
-  //     focused: id,
-  //   });
-  // }
+  componentDidMount() {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
+    
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
+
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+  
+  componentWillUnmount() {
+    this.socket.close();
+  }
+
   selectPanel(id) {
-    this.setState((previousState) => ({
-      focused: previousState.focused !== null ? null : id,
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
     }));
   }
 
   render() {
     const dashboardClasses = classnames("dashboard", {
-      "dashboard--focused": this.state.focused,
+      "dashboard--focused": this.state.focused
     });
 
-    if (this.state.loading) {
-      return <Loading />;
-    }
-
-    // Map over the data array and create a new Panel for each of the four data objects. Render the panels array as children of the main element.
     const panels = data
-      // Use the this.state.focused value to filter panel data before converting it to components.
-      .filter(
-        (panel) =>
-          this.state.focused === null || this.state.focused === panel.id
-      )
-      .map((panel) => (
-        <Panel
-          key={panel.id}
-          label={panel.label}
-          value={panel.value}
-          onSelect={(event) => this.selectPanel(panel.id)}
+      .filter( panel => this.state.focused === null || this.state.focused === panel.id )    
+      .map( (panel) => {
+        return <Panel 
+          key={panel.id} 
+          id={panel.id} 
+          label={panel.label} 
+          value={panel.getValue(this.state)}
+          onSelect={ () => this.selectPanel(panel.id)} 
         />
-      ));
+      });
+
+    if (this.state.loading) {
+      return <Loading />
+    }
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
