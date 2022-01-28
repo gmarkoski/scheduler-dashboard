79adb173d501853822587534bdfa2f42c90410fe
Gary <gary_markoski@hotmail.com>
2022-01-28 10:55:19 -0700

    initial commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,76 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6,
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm",
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday",
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3",
+  },
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  state = {
+    loading: false,
+    focused: null,
+  };
 
-    return <main className={dashboardClasses} />;
+  //We are intentionally using this form to highlight a pitfall. Make sure that the selectPanel method is written using the same syntax as below.
+  // selectPanel(id) {
+  //   this.setState({
+  //     focused: id,
+  //   });
+  // }
+  selectPanel(id) {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    // Map over the data array and create a new Panel for each of the four data objects. Render the panels array as children of the main element.
+    const panels = data
+      // Use the this.state.focused value to filter panel data before converting it to components.
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((panel) => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={(event) => this.selectPanel(panel.id)}
+        />
+      ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
